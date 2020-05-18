a2f4079489a079342c84350c55149eacac4e6ef4
Radek Altof <radek.altof@gmail.com>
2020-05-18 13:48:42 +0200

    Lint everything

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    './node_modules/@radek-altof/lint-config/eslint.json'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-	'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-	'vue/html-indent': [ 'error', 'tab' ]
-  }
-}
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/essential',
+		'./node_modules/@radek-altof/lint-config/eslint.json',
+	],
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'vue/html-indent': [ 'error', 'tab' ],
+	},
+};
