7d39ebd6570e99cb82a1372685531ac601553ce9
Radek Altof <radek.altof@gmail.com>
2020-07-21 10:47:14 +0200

    Define local i18n messages

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
+<i18n>
+{
+	"cs": {
+		"heading": "Tohle je stránka O nás."
+	},
+	"en": {
+		"heading": "This is an About page."
+	}
+}
+</i18n>
+
 <template>
 	<div class="about">
-		<h1>{{ $t('about.content') }}</h1>
+		<h1>{{ $t('heading') }}</h1>
+		<p>{{ $t('content') }}</p>
 	</div>
 </template>
+
+<script>
+export default {
+	name: 'About',
+	i18n: {
+		sharedMessages: {
+			cs: {
+				content: 'Zpráva načtená z i18n sharedMessages.',
+			},
+			en: {
+				content: 'A message loaded from i18n sharedMessages.',
+			},
+		},
+	},
+};
+</script>
